ea686c07cdc3a721fd1e97064984c7040585e406
YMac <yuu@CarmeloY.local>
2023-04-21 18:21:08 +0800

    updated

--- contracts/IDO.sol
+++ contracts/IDO.sol
@@ -12,6 +12,7 @@ contract IDO is Ownable, ReentrancyGuard {
     /////////////   错误提示  ///////////////////
     ////////////////////////////////////////////
     error AddressZero();
+    error NotExsit();
     error EarlyTime();
     error IDOisOver();
     error CallerIsNotUser();
@@ -46,7 +47,7 @@ contract IDO is Ownable, ReentrancyGuard {
     //计数参与人数量
     using Counters for Counters.Counter;
     Counters.Counter private _participantNumber;
-
+    //用于计算百分数
     using SafeMath for uint256;
 
     //总额度
@@ -102,10 +103,32 @@ contract IDO is Ownable, ReentrancyGuard {
         endTime = _newTime;
     }
 
+    //设定初始账号,用这个地址开始进行推广
+    function setFirstUser() external onlyOwner {
+        //存入用户信息
+        participant.push(
+            Participant({
+                selfAddress: msg.sender,
+                firstReferrerAddress: msg.sender,
+                secondReferrerAddress: msg.sender,
+                ido: false,
+                tokenAmount: 0,
+                withdrawal: false
+            })
+        );
+        //保存映射
+        addressToParticipant[msg.sender] = participant[0];
+    }
+
     //绑定上级
     function bindReferrer(address _referrerAddress) external callerIsUser {
         //不可为空地址
         if (_referrerAddress == address(0)) revert AddressZero();
+        //地址必须已注册
+        if (
+            addressToParticipant[_referrerAddress].selfAddress !=
+            _referrerAddress
+        ) revert NotExsit();
         //结束无法绑定
         if (endTime < block.timestamp) revert IDOisOver();
         //绑定一级推荐人; 如果绑定的上级也存在上级，则二级推荐人也会绑定
@@ -138,21 +161,18 @@ contract IDO is Ownable, ReentrancyGuard {
 
     //ido
     function ido(uint256 amount) external payable nonReentrant {
+        //限额 DAILYAMOUNT
+        //怎么写？？
+
         //参与额度必须为100、300、500
         if (
             amount != 100 * 1e18 || amount != 300 * 1e18 || amount != 500 * 1e18
         ) revert AmountError();
         IERC20 usdt = IERC20(address(usdtAddress));
-        //遍历找出该用户信息
-        Participant memory idoer;
-        uint256 i;
-        for (i = 0; i <= participant.length; i++) {
-            if (participant[i].selfAddress == msg.sender) {
-                idoer = participant[i];
-            }
-        }
+        //赋值用户信息
+        Participant memory idoer = addressToParticipant[msg.sender];
         //参与者ido状态必须为false
-        if (participant[i].ido == true) revert AlreadyIDO();
+        if (idoer.ido == true) revert AlreadyIDO();
         //推荐人返usdt的比例
         uint256 firstReferrerAmount = amount.mul(3).div(100);
         uint256 secondReferrerAmount = amount.mul(2).div(100);
@@ -169,26 +189,22 @@ contract IDO is Ownable, ReentrancyGuard {
             usdt.transfer(fundAddress, amount.sub(firstReferrerAmount));
         }
         //修改用户可提取token的数量
-        participant[i].tokenAmount = amount.div(IDOPRICE);
+        addressToParticipant[msg.sender].tokenAmount = amount.div(IDOPRICE);
         //修改参与状态, 默认false, true则不可参与
-        participant[i].ido = true;
+        addressToParticipant[msg.sender].ido = true;
         //记录参与人数+1
         _participantNumber.increment();
         participantNumber = _participantNumber.current();
+        //已购金额+amount
+        purchasedAmount += amount;
         //事件
         emit idoSuc(msg.sender, amount);
     }
 
     //用户提取token
     function tokenWithdraw() external nonReentrant {
-        //遍历找出该用户信息
-        Participant memory idoer;
-        uint256 i;
-        for (i = 0; i <= participant.length; i++) {
-            if (participant[i].selfAddress == msg.sender) {
-                idoer = participant[i];
-            }
-        }
+        //赋值用户信息
+        Participant memory idoer = addressToParticipant[msg.sender];
         //要求用户参与ido
         if (idoer.ido == false) revert NotIDO();
         //要求用户未提取过token
@@ -196,9 +212,9 @@ contract IDO is Ownable, ReentrancyGuard {
         //tokenAmount不为0
         if (idoer.tokenAmount == 0) revert NotEnoughToken();
         //修改用户token数量信息
-        participant[i].tokenAmount = 0;
+        addressToParticipant[msg.sender].tokenAmount = 0;
         //修改用户提取状态信息
-        participant[i].withdrawal = true;
+        addressToParticipant[msg.sender].withdrawal = true;
         //发送token给用户
         IERC20 customToken = IERC20(address(customTokenAddress));
         customToken.transferFrom(address(this), msg.sender, idoer.tokenAmount);
